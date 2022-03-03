8ba5771f24c0aa73fecd6ae93e5c11cc5aae949c
oliverdaniels-koch <danielskoch.oliver@gmail.com>
2022-03-02 20:05:25 -0500

    update parallel-trpo requirements (updated versions)

--- agents/parallel-trpo/setup.py
+++ agents/parallel-trpo/setup.py
@@ -9,9 +9,9 @@ if sys.version_info.major != 3:
 setup(name='parallel_trpo',
     version='0.0.1',
     install_requires=[
-        'gym[mujoco] ~= 0.9.2',
-        'mujoco-py ~= 0.5.7',
-        'multiprocess ~= 0.70.5'
+        'gym',
+        'mujoco-py',
+        'multiprocess'
     ],
     # https://github.com/tensorflow/tensorflow/issues/7166#issuecomment-280881808
     extras_require={
